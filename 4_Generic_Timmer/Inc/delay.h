3a1b6b538a23983919f3a779f605b7142ee525be
LeonelNL <leonelnieto305@gmail.com>
2024-03-03 22:49:22 -0600

    Add delay with Systick

---
+++ 4_Generic_Timmer/Inc/delay.h
@@ -0,0 +1,8 @@
+#ifndef DELAY_H
+#define DELAY_H
+
+#include <stdint.h>
+
+extern void SystickDelayMs(uint32_t delay);
+
+#endif
---
+++ 4_Generic_Timmer/Src/main.c
@@ -0,0 +1,26 @@
+#include <stdint.h>
+#include "interrupt.h"
+#include "delay.h"
+
+int main(void)
+{
+  RCC->AHB1ENR    |=  RCC_AHB1ENR_GPIOA_EN;                // Enable clock port A (Led)
+  RCC->AHB1ENR    |=  RCC_AHB1ENR_GPIOC_EN;                // Enable clock port C (Button)
+  GPIOA->MODER    &= ~(GPIO_MODER_05_OUTPUT);              // Clen registers port A
+  GPIOA->MODER    |=  GPIO_MODER_05_OUTPUT;                // Set output pin 5
+  GPIOC->MODER    &= ~(GPIO_MODER_INPUT);                  // Clean resgister port C    
+
+  RCC->APB2ENR    |= RCC_APB2ENR_SYSCFGEN_ClockEnabled;    // Enable clock System configuration
+  SYSCFG->EXTICR4 |= SYSCFG_EXTICR4_PC13;                  // Set the interrupt in PC13
+  EXTI->IMR       |= EXTI_IMR_MR13_ISNOTMASKED;            // Enable external interruption PC13
+  EXTI->FTSR      |= EXTI_FTSR_TR13_FALLING_TRIGGER_EN;    // Set the falling trigger enable 
+
+  enableInterruptIRQn(EXTI15_10_IRQn);
+
+
+  while (1)
+  {
+    GPIOA->ODR ^= (1<<5);
+    SystickDelayMs(1000);
+  }
+}
